a56e1fd245a0635d64f0c72266035c190e317868
duyên huynh <dh18062000@gmail.com>
2020-09-19 10:06:11 +0700

    reponsive changed

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 var icon= document.getElementById("show-icon");
 icon.addEventListener("click", showIcon);
 var menu= document.querySelector(".hidden");
+
 let count=0;
 window.addEventListener("load", function(){
     menu.classList.remove("show");
+   
 });
 function showIcon(){
     count+=1;
     menu.classList.add("show");
     
+    
     if(count%2==0){
         menu.classList.remove("show");
+        
 
     }
     
